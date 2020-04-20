763ee879730a0ad5a515011007c8702f57547eab
lfbxzlx <lfbxzlx@163.com>
2020-04-20 13:28:34 +0800

    添加跳过错误的情况

---
+++ clever_permission/src/main/java/com/example/clever_permission/PermissionSkipFailureCallBack.kt
@@ -0,0 +1,27 @@
+package com.example.cleverpermissionutil.permission
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+import com.example.clever_permission.RxPermissionUtils
+
+public abstract class PermissionSkipFailureCallBack : BasePermissionCallBack() {
+    //    AlertDialog.Builder alertDialog
+    lateinit var alertDialog: AlertDialog.Builder
+
+    override fun onRequestPermissionFailureWithAskNeverAgain(permissions: List<String>) {
+        onCreateDialog()
+        alertDialog.setTitle("提示")
+        alertDialog.setMessage("权限缺失，点击确定后前往设置页面设置权限")
+        alertDialog.setPositiveButton("确定") { dialogInterface: DialogInterface, i: Int ->
+            RxPermissionUtils.startSettingsActivity(mContext)
+        }.show()
+    }
+
+    private fun onCreateDialog() {
+        if (!::alertDialog.isInitialized)
+            alertDialog = AlertDialog.Builder(mContext)
+    }
+
+    val tag: String = javaClass.simpleName
+
+}
